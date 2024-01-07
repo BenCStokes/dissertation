d1cd508aa9455c032d170aed89476d645a4e311f
Ben Stokes <ben@benstokes.org.uk>
2024-01-07 21:07:13 +0000

    assign locations

--- bin/gen.ml
+++ bin/gen.ml
@@ -15,8 +15,47 @@ let next_location_name = function
   | 'z' -> failwith "End of alphabet reached (go add more names)"
   | letter -> Char.chr (1 + Char.code letter)
 
+let next_location (pa_id, alias_index) relation = match location_info relation with
+  | `Same -> (pa_id, alias_index)
+  | `Diff -> (pa_id + 1, 0)
+  | `Aliased -> (pa_id, alias_index + 1)
+
+let rotate n xs =
+  let rec go n = function
+    | [] -> raise (Invalid_argument "Rotated too far")
+    | (x::xs) ->
+      if n = 0 then
+        ((x::xs), [])
+      else
+        let (l, r) = go (n - 1) xs in (l, x::r) in
+  let (l, r) = go n xs in
+  l @ r
+
+let apply_rotation cycle =
+  let rec find_rotation_point index state = function
+    | [] -> state
+    | [rel] -> if location_info rel = `Same then state else None
+    | (rel::cycle) -> match location_info rel with
+      | `Same -> find_rotation_point (index+1) state cycle
+      | `Aliased -> find_rotation_point (index+1) (Some (Option.value state ~default:index)) cycle
+      | `Diff -> Some index in
+  let rotation_point = Option.value ~default:0 (find_rotation_point 1 None cycle) in
+  rotate rotation_point cycle
+
+let locations_from_cycle =
+  let rec go initial = function
+    | [] -> []
+    | (rel::cycle) -> initial :: go (next_location initial rel) cycle in
+  go (0, 0)
+
 let test_from_cycle cycle =
   let lhs_types = List.map lhs_type cycle in
   let rhs_types = List.map rhs_type cycle in
   if not (lhs_rhs_types_match lhs_types rhs_types) then raise BadCycle else
+  let cycle = apply_rotation cycle in
+  let locations = locations_from_cycle cycle in
   
+  (* Assign write serialisation order by physical address *)
+  (* Identify significant reads and put into final assertion *)
+  (* Set up registers initially storing virtual addresses *)
+  (* Add write serialisation constraints: condition for 2 writes to some pa, or observer thread for more *)
--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,10 @@
 let () = print_endline "Hello, World!"
 
+open Cycle
+open Gen
+
+let cycle = [FromRead (`Aliased, Internal); ProgramOrder (`Aliased, Write, Read)]
+
+let () = List.iter (fun (x,y) -> Printf.printf "(%d, %d)\n" x y) (test_from_cycle cycle)
+
 (*let () = let open Format in let open AArch64 in printf "%a" print_asm (LDR(Register 0,Register 1))*)
