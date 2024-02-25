ff8febfefe2cdd928fba53fa69d2d897eb58e7b5
Ben Stokes <ben@benstokes.org.uk>
2024-02-25 23:36:14 +0000

    output directly to file

--- bin/main.ml
+++ bin/main.ml
@@ -25,4 +25,7 @@ let () = Arg.parse args (fun arg -> print_endline ("Ignored anonymous argument: 
 module TestPrinter = Test.Printer(AArch64)
 let () =
   let test = test_from_cycle !test_name !orig !cycle in
-  Format.printf "%a\n" TestPrinter.pp_test test
+  let file_name = "generated/" ^ !test_name ^ ".litmus.toml" in
+  let out = if Sys.file_exists file_name then stdout else open_out file_name in
+  let formatter =  Format.formatter_of_out_channel out in
+  Format.fprintf formatter "%a\n" TestPrinter.pp_test test
