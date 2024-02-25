77fb5931e508f2e3ab8bfb703a2df88b703082c0
Ben Stokes <ben@benstokes.org.uk>
2024-02-25 21:39:37 +0000

    script to generate ben thesis tests

---
+++ benthesis/Generate.hs
@@ -0,0 +1,12 @@
+import System.Cmd (system)
+import Control.Monad (forM_, void)
+
+run line =
+  let (name, _:_:cycle) = span (/= ':') line in
+  system ("~/dissertation/_build/install/default/bin/dissertation -name " ++ name ++ " -cycle " ++ cycle ++ " > ~/dissertation/benthesis/new/" ++ name ++ ".litmus.toml")
+
+main = do
+  cycles <- lines <$> readFile "cycles"
+  forM_ cycles $ \line -> case line of
+    '#':_ -> return ()
+    _ -> void (run line)
