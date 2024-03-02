f0d6d16d5b436fcd8ac264bc431acc1e3fa37805
Ben Stokes <ben@benstokes.org.uk>
2024-03-02 14:06:03 +0000

    fixed generator script

--- benthesis/Generate.hs
+++ benthesis/Generate.hs
@@ -3,7 +3,7 @@ import Control.Monad (forM_, void)
 
 run line =
   let (name, _:_:cycle) = span (/= ':') line in
-  system ("~/dissertation/_build/install/default/bin/dissertation -name " ++ name ++ " -cycle " ++ cycle ++ " > ~/dissertation/benthesis/new/" ++ name ++ ".litmus.toml")
+  system ("~/dissertation/_build/install/default/bin/dissertation -stdout -name " ++ name ++ " -cycle " ++ cycle ++ " > ~/dissertation/benthesis/new/" ++ name ++ ".litmus.toml")
 
 main = do
   cycles <- lines <$> readFile "cycles"
--- bin/main.ml
+++ bin/main.ml
@@ -13,7 +13,9 @@ let usage = "testgen [-name <name>] -cycle <cycle>"
 let cycle = ref []
 let orig = ref ""
 let test_name = ref "PLACEHOLDER"
+let use_stdout = ref false
 let args = [
+  "-stdout", Arg.Set use_stdout, "Output the generated test to stdout";
   "-name", Arg.Set_string test_name, "The name of the test";
   "-cycle", Arg.Rest_all (fun tokens ->
     cycle := List.map Cycle.parse_relation tokens;
@@ -26,6 +28,6 @@ module TestPrinter = Test.Printer(AArch64)
 let () =
   let test = test_from_cycle !test_name !orig !cycle in
   let file_name = "generated/" ^ !test_name ^ ".litmus.toml" in
-  let out = if Sys.file_exists file_name then stdout else open_out file_name in
+  let out = if !use_stdout || Sys.file_exists file_name then stdout else open_out file_name in
   let formatter =  Format.formatter_of_out_channel out in
   Format.fprintf formatter "%a\n" TestPrinter.pp_test test
